4bb7a5e34654a582c2abd4a7698b4eb9fb8addba
Juan Antonio Osorio <jaosorior@gmail.com>
2011-10-08 01:59:41 -0500

    Added the function that's supposed to determine the tuple.
    It's incomplete still...

--- mod/xt_nat64.c
+++ mod/xt_nat64.c
@@ -12,6 +12,10 @@
 #include <linux/skbuff.h>
 #include <net/ipv6.h>
 #include <net/netfilter/nf_conntrack.h>
+#include <net/netfilter/nf_conntrack_core.h>
+#include <net/netfilter/nf_conntrack_proto.h>
+#include <net/netfilter/nf_conntrack_l3proto.h>
+#include <net/netfilter/nf_conntrack_l4proto.h>
 #include <net/netfilter/nf_nat.h>
 
 #include "nf_nat64_bib.h"
@@ -24,83 +28,109 @@ MODULE_DESCRIPTION("Xtables: RFC 6146 \"NAT64\" implementation");
 MODULE_ALIAS("ipt_nat64");
 MODULE_ALIAS("ip6t_nat64");
 
-static bool nat64_tg6_cmp(struct ipv6hdr *iph, const struct xt_nat64_tginfo *info)
+#define IPV6_HDRLEN 40
+static struct nf_conntrack_l3proto *l3proto_ip __read_mostly;
+static struct nf_conntrack_l3proto *l3proto_ipv6 __read_mostly;
+
+/*
+ * IPv6 comparison function. It's use as a call from nat64_tg6 is to compare
+ * the incoming packet's ip with the rule's ip, and so when the module is in
+ * debugging mode it prints the rule's IP.
+ */
+static bool nat64_tg6_cmp(struct in6_addr * ip_a, struct in6_addr * ip_b,
+		struct in6_addr * ip_mask)
 {
-	pr_debug("\n* ICNOMING IPV6 PACKET COMPARISON *\n");
-	pr_debug("PKT SRC=%pI6 \n", &iph->saddr);
-	pr_debug("PKT DST=%pI6 \n", &iph->daddr);
 
 	if (info->flags & XT_NAT64_IPV6_DST) {
-		pr_debug("RULE DST=%pI6 \n", &info->ip6dst.in6);
-		pr_debug("RULE DST_MSK=%pI6 \n", &info->ip6dst_mask.in6);
-
-		if (ipv6_masked_addr_cmp(&info->ip6dst.in6, &info->ip6dst_mask.in6,
-					&iph->saddr) != 0) 
-			pr_debug("Comparison returned true\n");
+		if (ipv6_masked_addr_cmp(ip_a, ip_mask, ip_b) != 0) 
+			pr_debug("NAT64: IPv6 comparison returned true\n");
 			return true;
 	}
 
-	pr_debug("Comparison returned false\n");
+	pr_debug("NAT64: IPv6 comparison returned false\n");
 	return false;
 }
 
+static bool nat64_determine_tuple(u_int8_t l3protocol, u_int8_t l4protocol, 
+		struct sk_buff *skb, union nf_inet_ipaddr addr)
+{
+	const struct nf_conntrack_l4_proto *l4proto;
+	struct nf_conntrack_tuple inner, target;
+	int l3_hdrlen, l4_hdrlen;
+	if (l3protocol == NFPROTO_IPV4)
+		l3_hdrlen = ip_hdrlen(skb);
+	else
+		l3_hdrlen = IPV6_HDRLEN;
+
+	inner =  memset(inner, 0, sizeof(*inner));
+
+	l4proto = __nf_ct_l4proto_find(l3protocol, l4protocol);
+
+	if (l4_protocol == IPPROTO_TCP)
+
+	if (!nf_ct_get_tuple(skb, hdrlen + sizeof(struct icmphdr),
+				(hdrlen +
+				 sizeof(struct icmphdr) + inside->ip.ihl * 4),
+				(u_int16_t)l3protocol, l4protocol,
+				&inner, l3proto, l4proto))
+		return 0;
+}
+
+/*
+ * IPv4 entry function
+ *
+ */
 static unsigned int nat64_tg4(struct sk_buff *skb, const struct xt_action_param *par)
 {
-	//const struct xt_nat64_tginfo *info = par->targinfo;
+	union nf_inet_addr;
 	struct iphdr *iph = ip_hdr(skb);
-	//struct nf_nat_range range = {};
-	//struct nf_conn *ct;
-
-	/* for debugging */
-	//ct = nf_ct_get(skb, &ctinfo);
-	/* 
-	 * For an IPv4 packet to enter a connection needs to be attached to it
-	 * already
-	 */
-	//NF_CT_ASSERT(ct != NULL && (ctinfo == IP_CT_RELATED));
-	//range.flags = IP_NAT_RANGE_MAP_IPS;
-	//range.min_ip = ntohl(iph->saddr) & 
+	__u8 l4_protocol = iph->protocol;
 
 	pr_debug("\n* ICNOMING IPV4 PACKET *\n");
 	pr_debug("Drop it\n");
-	pr_debug("Protocol: ");
-
-	if (iph->protocol == IPPROTO_TCP)
-		pr_debug("TCP\n");
-	else if (iph->protocol == IPPROTO_UDP)
-		pr_debug("UDP\n");
-	else if (iph->protocol == IPPROTO_ICMP)
-		pr_debug("ICMP\n");
 
 	return NF_DROP;
 }
 
+/*
+ * IPv6 entry function
+ *
+ */
 static unsigned int nat64_tg6(struct sk_buff *skb, const struct xt_action_param *par)
 {
+	union nf_inet_addr;
 	const struct xt_nat64_tginfo *info = par->targinfo;
 	struct ipv6hdr *iph = ipv6_hdr(skb);
-	//struct nf_conn *ct;
-	bool accept = nat64_tg6_cmp(iph, info);
+	__u8 l4_protocol = iph->nexthdr;
 
-	if (!accept)
+	pr_debug("\n* ICNOMING IPV6 PACKET *\n");
+	pr_debug("PKT SRC=%pI6 \n", &iph->saddr);
+	pr_debug("PKT DST=%pI6 \n", &iph->daddr);
+	pr_debug("RULE DST=%pI6 \n", &info->ip6dst.in6);
+	pr_debug("RULE DST_MSK=%pI6 \n", &info->ip6dst_mask);
+
+	if (!nat64_tg6_cmp(&info->ip6dst.in6, &info->ip6dst_mask.in6, &iph->saddr))
 		return NF_DROP;
 
-	pr_debug("Protocol: ");
+	if (l4_protocol & NAT64_IPV6_ALLWD_PROTOS)
+		if(nat64_determine_tuple(NFPROTO_IPV6, l4_protocol, &skb, ))
+			pr_debug("NAT64: Determining the tuple stage went OK.");
+		else
+			pr_debug("NAT64: Something went wrong in the determining the tuple"
+					"stage.");
 
-	if (iph->nexthdr == IPPROTO_TCP)
-		pr_debug("TCP\n");
-	else if (iph->nexthdr == IPPROTO_UDP)
-		pr_debug("UDP\n");
-	else if (iph->nexthdr == IPPROTO_ICMP)
-		pr_debug("ICMP\n");
-	else if (iph->nexthdr == IPPROTO_ICMPV6)
-		pr_debug("ICMPV6\n");
-	else
-		return NF_DROP;
+	return NF_DROP;
 
-	return NF_ACCEPT;
 }
 
+/*
+ * General entry point. 
+ *
+ * Here the NAT64 implementation validates that the
+ * incoming packet is IPv4 or IPv6. If it isn't, it silently drops the packet.
+ * If it's one of those two, it calls it's respective function, since the IPv6
+ * header is handled differently than an IPv4 header.
+ */
 static unsigned int nat64_tg(struct sk_buff *skb, const struct xt_action_param *par)
 {
 	if (par->family == NFPROTO_IPV4)
@@ -136,11 +166,15 @@ static struct xt_target nat64_tg_reg __read_mostly = {
 
 static int __init nat64_init(void)
 {
+	l3proto_ip = nf_ct_l3proto_find_get((u_int16_t)NFPROTO_IPV4);
+	l3proto_ipv6 = nf_ct_l3proto_find_get((u_int16_t) NFPROTO_IPV6);
 	return xt_register_targets(nat64_tg_reg, ARRAY_SIZE(nat64_tg_reg));
 }
 
 static void __exit nat64_exit(void)
 {
+	nf_ct_l3proto_put(l3proto_ip);
+	nf_ct_l3proto_put(l3proto_ipv6);
 	xt_unregister_targets(nat64_tg_reg, ARRAY_SIZE(nat64_tg_reg));
 }
 
