519b4f11706d4ce9c8d66cb7d2cd30bdcc8ffba1
Luis Hinojosa <lf.hinojosa@gmail.com>
2011-10-25 22:27:06 -0500

    first experiment backup after huge failure

---
+++ mod/workflow.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $1 = "ins" ] ; then
+	sudo modprobe ipv6
+	sudo modprobe ip_tables
+	sudo modprobe nf_conntrack
+	sudo modprobe nf_conntrack_ipv4
+	sudo ip6tables -t mangle --flush
+	sudo insmod xt_nat64.ko
+	echo "installed the NAT64 module"
+elif [ $1 = "rmv" ] ; then
+	sudo ip6tables -t mangle --flush
+	sudo rmmod xt_nat64.ko
+	echo "removed the NAT64 module"
+elif [ $1 = "debug" ] ; then
+	sudo dmesg | tail -30
+elif [ $1 = "test" ] ; then
+	sudo ip6tables -t mangle --flush
+	sudo ip6tables -t mangle -A PREROUTING -j nat64 --ipdst ::1/64;
+	sudo ip6tables -t mangle -n -L
+	sudo ping6 ::1
+elif [ $1 = "setup" ] ; then
+	sudo ./workflow.sh "ins"
+	sudo ./workflow.sh "test"
+	sudo ./workflow.sh "rmv"
+	sudo ./workflow.sh "ins"
+else
+	echo "no valid action selected" 
+fi
