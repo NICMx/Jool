abcec86bb3a3858188a4f3936d1e0a1991e728fe
Miguel Gonzalez <maggonzz@gmail.com>
2012-02-15 22:46:34 -0600

    second commit

---
+++ testing/setup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+ifconfig eth1 add fec0:24::c0a8:0180/64
+ip -6 addr del fe80::204:e2ff:fe25:6ab4/64 dev eth0
+ip -6 addr del fe80::221:70ff:fe06:2916/64 dev eth1
+ip route del fe80::/64
+
