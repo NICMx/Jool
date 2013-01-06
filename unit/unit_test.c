a3a4324d427a1e27c8b1d37806bb8e65bb02129a
Alberto Leiva Popper <aleiva@nic.mx>
2013-01-06 17:46:49 -0600

    Addressing TODOs. - The hdr iterator will now stop if it reaches the packet's end. - Translate packet's overall structure is now more consistent with core's. - Assert functions are macros no more; they evaluated some parameters more than once, which was prone to bugs. Units no longer panic too quickly as a side effect. - Improved an added tests. Found (not fixed) errors in the outgoing module.

---
+++ unit/unit_test.c
@@ -0,0 +1,115 @@
+#include <linux/kernel.h>
+#include "unit_test.h"
+
+#define UNIT_WARNING(test_name, expected, actual, specifier) \
+		log_warning("Test '%s' failed. Expected: " specifier ". Actual: " specifier ".", \
+				test_name, \
+				expected, \
+				actual);
+#define UNIT_WARNING_NOT(test_name, expected, actual, specifier) \
+		log_warning("Test '%s' failed. Expected: not " specifier ". Actual: " specifier ".", \
+				test_name, \
+				expected, \
+				actual);
+
+
+bool assert_true(bool condition, char *test_name)
+{
+	if (!condition) {
+		log_warning("Test '%s' failed.", test_name);
+		return false;
+	}
+	return true;
+}
+
+bool assert_equals_int(int expected, int actual, char *test_name)
+{
+	if (expected != actual) {
+		UNIT_WARNING(test_name, expected, actual, "%d");
+		return false;
+	}
+	return true;
+}
+
+bool assert_equals_u8(__u8 expected, __u8 actual, char *test_name)
+{
+	return assert_equals_u32(expected, actual, test_name);
+}
+
+bool assert_equals_u16(__u16 expected, __u16 actual, char *test_name)
+{
+	return assert_equals_u32(expected, actual, test_name);
+}
+
+bool assert_equals_u32(__u32 expected, __u32 actual, char *test_name)
+{
+	if (expected != actual) {
+		UNIT_WARNING(test_name, expected, actual, "%u");
+		return false;
+	}
+	return true;
+}
+
+bool assert_equals_ptr(void *expected, void *actual, char *test_name)
+{
+	if (expected != actual) {
+		UNIT_WARNING(test_name, expected, actual, "%p");
+		return false;
+	}
+	return true;
+}
+
+bool assert_equals_ipv4(struct in_addr *expected, struct in_addr *actual, char *test_name)
+{
+	if (!ipv4_addr_equals(expected, actual)) {
+		UNIT_WARNING(test_name, expected, actual, "%pI4");
+		return false;
+	}
+	return true;
+}
+
+bool assert_equals_ipv6(struct in6_addr *expected, struct in6_addr *actual, char *test_name)
+{
+	if (!ipv6_addr_equals(expected, actual)) {
+		UNIT_WARNING(test_name, expected, actual, "%pI6c");
+		return false;
+	}
+	return true;
+}
+
+bool assert_null(void *actual, char *test_name)
+{
+	return assert_equals_ptr(NULL, actual, test_name);
+}
+
+bool assert_false(bool condition, char *test_name)
+{
+	if (condition) {
+		log_warning("Test '%s' failed.", test_name);
+		return false;
+	}
+	return true;
+}
+
+bool assert_not_equals_u16(__u16 expected, __u16 actual, char *test_name)
+{
+	if (expected == actual) {
+		UNIT_WARNING_NOT(test_name, expected, actual, "%u");
+		return false;
+	}
+	return true;
+}
+
+bool assert_not_equals_ptr(void *expected, void *actual, char *test_name)
+{
+	if (expected == actual) {
+		UNIT_WARNING_NOT(test_name, expected, actual, "%p");
+		return false;
+	}
+	return true;
+}
+
+bool assert_not_null(void *actual, char *test_name)
+{
+	return assert_not_equals_ptr(NULL, actual, test_name);
+}
