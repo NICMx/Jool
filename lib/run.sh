b9826960d80432015bf8d6951a38379501a83684
Alberto Leiva Popper <ydahhrk@gmail.com>
2013-04-02 16:33:56 -0600

    Removing debris we forgot to clean when we moved from iptables to netfilter. Deleted the iptables library and updated the installation instructions.

--- lib/run.sh
+++
@@ -1,7 +0,0 @@
-make libxt_NAT64.so
-# As for that directory, your mileage might vary. Other candidates are
-# /usr/lib/iptables
-# /usr/lib/xtables
-# /usr/local/lib/xtables
-# etc
-sudo mv libxt_NAT64.so /lib/xtables
