ddda74749c8ff40c95e83569a3038c607e56dfce
Miguel Gonzalez <maggonzz@gmail.com>
2014-12-11 11:27:53 -0600

    fixed typos

--- mod/config.c
+++ mod/config.c
@@ -220,10 +220,10 @@ static int handle_pool4_config(struct nlmsghdr *nl_hdr, struct request_hdr *nat6
 			return respond_error(nl_hdr, -EPERM);
 
 		log_debug("Adding an address to the IPv4 pool.");
-		if (request->add.maskbits == NULL)
+		if (&request->add.maskbits == NULL)
 			return respond_error(nl_hdr, pool4_register(&request->add.addr));
 		else
-			return respond_error(pool4_cidr_range(request->add.addr,request->add.maskbits));
+			return respond_error(nl_hdr,pool4_cidr_range(request->add.addr,request->add.maskbits));
 	case OP_REMOVE:
 		if (verify_superpriv(nat64_hdr))
 			return respond_error(nl_hdr, -EPERM);
