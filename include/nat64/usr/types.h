b9897bcf7f97e8705be2662b4e1729c4925303a1
Roberto Olivas <rolivas@nic.mx>
2015-09-04 12:45:26 -0500

    changes were made, to send log_err to stderr stream.

--- include/nat64/usr/types.h
+++ include/nat64/usr/types.h
@@ -6,7 +6,7 @@
 
 #define log_debug(text, ...) printf(text "\n", ##__VA_ARGS__)
 #define log_info(text, ...) log_debug(text, ##__VA_ARGS__)
-#define log_err(text, ...) log_debug(text, ##__VA_ARGS__)
+#define log_err(text, ...) fprintf(stderr, text "\n", ##__VA_ARGS__)
 
 
 #endif /* _JOOL_USR_TYPES_H */
