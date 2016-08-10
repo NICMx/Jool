NICMx/Jool

commit b06e2448aa18b6f006ead74c09e3643afa0ad7e4

More misc review and testing
Also fixes the atomic fragments part of #221.

include/nat64/common/xlat.h
@@ -13,7 +13,7 @@
 #define JOOL_VERSION_MAJOR 3
 #define JOOL_VERSION_MINOR 4
 #define JOOL_VERSION_REV 2
-#define JOOL_VERSION_DEV 10
+#define JOOL_VERSION_DEV 11
 
 /** See http://stackoverflow.com/questions/195975 */
 #define STR_VALUE(arg) #arg

mod/common/rfc6145/4to6.c
@@ -375,7 +375,6 @@ static __be32 icmp6_minimum_mtu(struct xlation *state,
 		unsigned int nexthop4_mtu,
 		__u16 tot_len_field)
 {
-	bool lower_mtu_fail;
 	__u32 result;
 
 	if (packet_mtu == 0) {
@@ -396,21 +395,10 @@ static __be32 icmp6_minimum_mtu(struct xlation *state,
 		}
 	}
 
-	packet_mtu += 20;
-	nexthop4_mtu += 20;
-
-	/* Core comparison to find the minimum value. */
-	result = min(packet_mtu, min(nexthop6_mtu, nexthop4_mtu));
-
-	lower_mtu_fail = state->jool.global->cfg.atomic_frags.lower_mtu_fail;
-	if (lower_mtu_fail && result < IPV6_MIN_MTU) {
-		/*
-		 * Probably some router does not implement RFC 4890, section
-		 * 4.3.1. Gotta override and hope for the best.
-		 * See RFC 6145 section 6, second approach.
-		 */
+	/* Here's the core comparison. */
+	result = min(packet_mtu + 20, min(nexthop6_mtu, nexthop4_mtu + 20));
+	if (result < IPV6_MIN_MTU)
 		result = IPV6_MIN_MTU;
-	}
 
 	return cpu_to_be32(result);
 }

mod/common/rfc6145/common.c
@@ -83,24 +83,9 @@ int copy_payload(struct xlation *state)
 	return error;
 }
 
-static bool build_ipv6_frag_hdr(struct xlation *state)
-{
-	if (is_df_set(pkt_ip4_hdr(&state->in)))
-		return false;
-
-	return state->jool.global->cfg.atomic_frags.build_ipv6_fh;
-}
-
 bool will_need_frag_hdr(struct xlation *state)
 {
-	/*
-	 * Note, build_ipv6_frag_hdr(in_hdr) should remain disabled.
-	 * See https://www.jool.mx/en/usr-flags-atomic.html
-	 * (if that's down, try doc/usr/usr-flags-atomic.md in Jool's source.)
-	 */
-	return build_ipv6_frag_hdr(state)
-			|| is_mf_set_ipv4(pkt_ip4_hdr(&state->in))
-			|| get_fragment_offset_ipv4(pkt_ip4_hdr(&state->in));
+	return get_fragment_offset_ipv4(pkt_ip4_hdr(&state->in));
 }
 
 static int move_pointers_in(struct packet *pkt, __u8 protocol,

mod/stateful/Makefile
@@ -48,5 +48,4 @@ again:
 	sudo make modules_install
 	sudo modprobe jool pool6=64:ff9b::/96
 	sudo jool -bat 1::5#65000 192.0.2.1#65000
-	sudo jool --address-dependent-filtering 1
 	sudo dmesg -C

mod/stateful/bib/db.c
@@ -328,10 +328,12 @@ static void init_table(struct bib_table *table,
 	table->bib_count = 0;
 	table->session_count = 0;
 	spin_lock_init(&table->lock);
-	init_expirer(&table->est_timer, est_timeout, true, est_cb);
+	init_expirer(&table->est_timer, est_timeout, SESSION_TIMER_EST, est_cb);
 
-	init_expirer(&table->trans_timer, trans_timeout, false, just_die);
-	init_expirer(&table->syn4_timer, TCP_INCOMING_SYN, false, just_die);
+	init_expirer(&table->trans_timer, trans_timeout, SESSION_TIMER_TRANS,
+			just_die);
+	init_expirer(&table->syn4_timer, TCP_INCOMING_SYN, SESSION_TIMER_SYN4,
+			just_die);
 	table->pkt_count = 0;
 	table->pkt_limit = 0;
 	table->drop_v4_syn = DEFAULT_DROP_EXTERNAL_CONNECTIONS;

test/unit/impersonator/bib.c
@@ -29,6 +29,11 @@ int pktqueue_add(struct pktqueue *queue, struct packet *pkt,
 	return broken_unit_call(__func__);
 }
 
+void pktqueue_rm(struct pktqueue *queue, struct ipv4_transport_addr *src4)
+{
+	/* No code. */
+}
+
 struct pktqueue_session *pktqueue_find(struct pktqueue *queue,
 		struct ipv6_transport_addr *addr,
 		struct mask_domain *masks)

commit b471bab647c50f11884cbad20308bb57c4858c0b

Misc tweaks:
- Fix #216: "if the established IPv4 mask is no longer a pool4
  candidate for the source (IPv6) client the BIB entry is
  recomputed from scratch.", only on empty pool4 translations.
- Improve validation of colliding EAMs so error messages during
  adds are more intuitive:
  https://mail-lists.nic.mx/pipermail/jool-list/2016-August/000070.html
  (full collisions are now validated before overlappings.)
- Type 2 packets are ICMP errored when their mother BIB entry dies.
- Fix incorrect bib count on --bib --count after adding/removing a
  static BIB entry.
- Fix fragmentation due to an error introduced in
  b06e2448aa18b6f006ead74c09e3643afa0ad7e4.
  (Was not adding a fragment header to the first fragment in 4->6)

include/nat64/common/xlat.h
@@ -13,7 +13,7 @@
 #define JOOL_VERSION_MAJOR 3
 #define JOOL_VERSION_MINOR 4
 #define JOOL_VERSION_REV 2
-#define JOOL_VERSION_DEV 11
+#define JOOL_VERSION_DEV 12
 
 /** See http://stackoverflow.com/questions/195975 */
 #define STR_VALUE(arg) #arg

include/nat64/mod/stateful/pool4/db.h
@@ -51,6 +51,7 @@ int mask_domain_next(struct mask_domain *masks,
 		bool *consecutive);
 bool mask_domain_matches(struct mask_domain *masks,
 		struct ipv4_transport_addr *addr);
+bool mask_domain_is_dynamic(struct mask_domain *masks);
 
 /*
  * Test functions (Illegal in production code)

mod/common/nl/bib.c
@@ -91,7 +91,7 @@ static int handle_bib_add(struct xlator *jool, struct request_bib *request)
 	switch (error) {
 	case 0:
 		break;
-	case -ESRCH:
+	case -EEXIST:
 		log_err("Entry %pI4#%u|%pI6c#%u collides with %pI4#%u|%pI6c#%u.",
 				&new.ipv4.l3, new.ipv4.l4,
 				&new.ipv6.l3, new.ipv6.l4,

mod/common/pool6.c
@@ -276,7 +276,7 @@ int pool6_add(struct pool6 *pool, struct ipv6_prefix *prefix)
 		 * TODO (4.0.0) remember to turn pool6 into a single global
 		 * param. This whole module being a linked list is stupid.
 		 */
-		log_err("SIIT Jool only supports one pool6 prefix at a time.");
+		log_err("Only one pool6 prefix can exist per Jool instance.");
 		return -EINVAL;
 	}
 

mod/common/rfc6145/4to6.c
@@ -141,6 +141,7 @@ static verdict generate_addr6_siit(struct xlation *state,
 	int error;
 
 	if (addr4_is_scope_subnet(addr4)) {
+		/* TODO this might trigger RFC 6791 and end up being successful. */
 		log_debug("Address %pI4 is not supposed to be xlat'd.", &tmp);
 		return VERDICT_ACCEPT;
 	}

mod/common/rfc6145/common.c
@@ -85,7 +85,8 @@ int copy_payload(struct xlation *state)
 
 bool will_need_frag_hdr(struct xlation *state)
 {
-	return get_fragment_offset_ipv4(pkt_ip4_hdr(&state->in));
+	return is_mf_set_ipv4(pkt_ip4_hdr(&state->in))
+			|| get_fragment_offset_ipv4(pkt_ip4_hdr(&state->in));
 }
 
 static int move_pointers_in(struct packet *pkt, __u8 protocol,

mod/stateful/Makefile
@@ -22,30 +22,17 @@ debug:
 # The targets below are meant for quick testing.
 # If you want to install the module, see ../INSTALL.
 
-insert:
-	# Don't need this noise.
-	sudo dmesg -C
-
-	# Insert the dependencies.
-	sudo modprobe nf_defrag_ipv6
-	sudo modprobe nf_defrag_ipv4
-
-	# Insert and configure Jool a little bit.
-	sudo insmod jool.ko pool6=64:ff9b::/96
-	sudo ../../usr/stateful/jool -4a 192.0.2.2 1-3000
-	sudo ../../usr/stateful/jool -batu 192.0.2.2#2000 2001:db8::5#2000
-	sudo ../../usr/stateful/jool -bai 192.0.2.2#1 2001:db8::5#1
-	dmesg
-
-remove:
-	-sudo rmmod jool.ko
-	-sudo modprobe -r nf_defrag_ipv6
-	-sudo modprobe -r nf_defrag_ipv4
-	dmesg | tail -1
 again:
 	sudo modprobe -r jool
 	make debug
 	sudo make modules_install
 	sudo modprobe jool pool6=64:ff9b::/96
-	sudo jool -bat 1::5#65000 192.0.2.1#65000
+	sudo jool -batu 1::5#65000 192.0.2.1#65000
+	#sudo jool --address-dependent-filtering on
+	sudo dmesg -C
+quick:
+	sudo modprobe -r jool
+	sudo modprobe jool pool6=64:ff9b::/96
+	sudo jool -batu 1::5#65000 192.0.2.1#65000
+	#sudo jool --address-dependent-filtering on
 	sudo dmesg -C

mod/stateful/bib/db.c
@@ -374,15 +374,29 @@ void bib_get(struct bib *db)
 	kref_get(&db->refs);
 }
 
+/**
+ * Potentially includes a laggy packet fetch; please do not hold spinlocks while
+ * calling this function!
+ */
 static void release_session(struct rb_node *node, void *arg)
 {
 	struct tabled_session *session = node2session(node);
+
+	if (session->stored) {
+		icmp64_send4(session->stored, ICMPERR_PORT_UNREACHABLE, 0);
+		kfree_skb(session->stored);
+	}
+
 	kmem_cache_free(session_cache, session);
 }
 
+/**
+ * Potentially includes laggy packet fetches; please do not hold spinlocks while
+ * calling this function!
+ */
 static void release_bib_entry(struct rb_node *node, void *arg)
 {
-	struct tabled_bib *bib = bib6_entry(node);
+	struct tabled_bib *bib = bib4_entry(node);
 	rbtree_clear(&bib->sessions, release_session, NULL);
 	kmem_cache_free(bib_cache, bib);
 }
@@ -396,9 +410,9 @@ static void release_bib(struct kref *refs)
 	 * The trees share the entries, so only one tree of each protocol
 	 * needs to be emptied.
 	 */
-	rbtree_clear(&db->udp.tree6, release_bib_entry, NULL);
-	rbtree_clear(&db->tcp.tree6, release_bib_entry, NULL);
-	rbtree_clear(&db->icmp.tree6, release_bib_entry, NULL);
+	rbtree_clear(&db->udp.tree4, release_bib_entry, NULL);
+	rbtree_clear(&db->tcp.tree4, release_bib_entry, NULL);
+	rbtree_clear(&db->icmp.tree4, release_bib_entry, NULL);
 
 	pktqueue_destroy(db->tcp.pkt_queue);
 
@@ -1126,6 +1140,60 @@ static int commit_add(struct bib_table *table,
 	return 0;
 }
 
+struct detach_args {
+	struct bib_table *table;
+	struct sk_buff *probes;
+	unsigned int detached;
+};
+
+static void detach_session(struct rb_node *node, void *arg)
+{
+	struct tabled_session *session = node2session(node);
+	struct detach_args *args = arg;
+
+	list_del(&session->list_hook);
+	if (session->stored)
+		args->table->pkt_count--;
+	args->detached++;
+}
+
+static unsigned int detach_sessions(struct bib_table *table,
+		struct tabled_bib *bib)
+{
+	struct detach_args arg = { .table = table, .detached = 0, };
+	rbtree_foreach(&bib->sessions, detach_session, &arg);
+	return arg.detached;
+}
+
+static void detach_bib(struct bib_table *table, struct tabled_bib *bib)
+{
+	rb_erase(&bib->hook6, &table->tree6);
+	rb_erase(&bib->hook4, &table->tree4);
+	table->bib_count--;
+	table->session_count -= detach_sessions(table, bib);
+}
+
+struct bib_delete_list {
+	struct rb_node *first;
+};
+
+static void add_to_delete_list(struct bib_delete_list *list, struct rb_node *node)
+{
+	node->rb_right = list->first;
+	list->first = node;
+}
+
+static void commit_delete_list(struct bib_delete_list *list)
+{
+	struct rb_node *node;
+	struct rb_node *next;
+
+	for (node = list->first; node; node = next) {
+		next = node->rb_right;
+		release_bib_entry(node, NULL);
+	}
+}
+
 /**
  * Tests whether @predecessor's immediate succesor tree slot is a suitable
  * placeholder for @bib. Returns the colliding node.
@@ -1231,6 +1299,9 @@ static int upgrade_pktqueue_session(struct bib_table *table,
 	struct tree_slot bib_slot4;
 	int error;
 
+	if (new->bib->proto != L4PROTO_TCP)
+		return -ESRCH;
+
 	sos = pktqueue_find(table->pkt_queue, &new->session->dst6, masks);
 	if (!sos)
 		return -ESRCH;
@@ -1293,6 +1364,13 @@ trainwreck:
 	return -EINVAL;
 }
 
+static bool issue216_needed(struct mask_domain *masks,
+		struct bib_session_tuple *old)
+{
+	return mask_domain_is_dynamic(masks)
+			&& !mask_domain_matches(masks, &old->bib->src4);
+}
+
 /**
  * This is a find and an add at the same time, for both @new->bib and
  * @new->session.
@@ -1308,25 +1386,42 @@ static int find_bib_session6(struct bib_table *table,
 		struct mask_domain *masks,
 		struct bib_session_tuple *new,
 		struct bib_session_tuple *old,
-		struct slot_group *slots)
+		struct slot_group *slots,
+		struct bib_delete_list *rm_list)
 {
 	int error;
 
 	old->bib = find_bibtree6_slot(table, new->bib, &slots->bib6);
 	if (old->bib) {
-		old->session = find_session_slot(old->bib, new->session, NULL,
-				&slots->session);
-		return 0;
-	}
+		if (!issue216_needed(masks, old)) {
+			old->session = find_session_slot(old->bib, new->session,
+					NULL, &slots->session);
+			return 0; /* Typical happy path for existing sessions */
+		}
 
-	/* Note: If old->bib is NULL, then old->session is also NULL. */
+		/*
+		 * Issue #216:
+		 * If pool4 was empty (when @masks was generated) and the BIB
+		 * entry's IPv4 address is no longer a mask candidate, drop the
+		 * BIB entry and recompute it from scratch.
+		 * https://github.com/NICMx/Jool/issues/216
+		 */
+		log_debug("Issue #216.");
+		detach_bib(table, old->bib);
+		add_to_delete_list(rm_list, &old->bib->hook4);
+		old->bib = NULL;
 
-	/* No BIB nor session in the main database? Try the SO sub-database. */
-	if (new->bib->proto == L4PROTO_TCP) {
+	} else {
+		/*
+		 * No BIB nor session in the main database? Try the SO
+		 * sub-database.
+		 */
 		error = upgrade_pktqueue_session(table, masks, new, old);
 		if (!error)
 			return 0;
 	}
+
+	/* Note: If old->bib is NULL, then old->session is also NULL. */
 
 	/* No collisions (find failed); try to find suitable slots (add) */
 	if (masks) {
@@ -1365,7 +1460,7 @@ static int find_bib_session6(struct bib_table *table,
 			&new->session->tree_hook);
 	old->session = NULL;
 
-	return 0;
+	return 0; /* Happy path for new sessions */
 }
 
 /**
@@ -1387,6 +1482,7 @@ int bib_add6(struct bib *db,
 	struct bib_session_tuple new;
 	struct bib_session_tuple old;
 	struct slot_group slots;
+	struct bib_delete_list rm_list = { NULL };
 	int error;
 
 	table = get_table(db, tuple6->l4_proto);
@@ -1412,7 +1508,7 @@ int bib_add6(struct bib *db,
 
 	spin_lock_bh(&table->lock); /* Here goes... */
 
-	error = find_bib_session6(table, masks, &new, &old, &slots);
+	error = find_bib_session6(table, masks, &new, &old, &slots, &rm_list);
 	if (error)
 		goto end;
 
@@ -1433,6 +1529,7 @@ end:
 		kmem_cache_free(bib_cache, new.bib);
 	if (new.session)
 		kmem_cache_free(session_cache, new.session);
+	commit_delete_list(&rm_list);
 
 	return error;
 }
@@ -1523,6 +1620,7 @@ verdict bib_add_tcp6(struct bib *db,
 	struct bib_session_tuple new;
 	struct bib_session_tuple old;
 	struct slot_group slots;
+	struct bib_delete_list rm_list = { NULL };
 	verdict verdict;
 
 	if (WARN(pkt->tuple.l4_proto != L4PROTO_TCP, "Incorrect l4 proto in TCP handler."))
@@ -1534,7 +1632,7 @@ verdict bib_add_tcp6(struct bib *db,
 	table = &db->tcp;
 	spin_lock_bh(&table->lock);
 
-	if (find_bib_session6(table, masks, &new, &old, &slots)) {
+	if (find_bib_session6(table, masks, &new, &old, &slots, &rm_list)) {
 		verdict = VERDICT_DROP;
 		goto end;
 	}
@@ -1573,6 +1671,7 @@ end:
 		kmem_cache_free(bib_cache, new.bib);
 	if (new.session)
 		kmem_cache_free(session_cache, new.session);
+	commit_delete_list(&rm_list);
 
 	return verdict;
 }
@@ -1734,6 +1833,7 @@ int bib_add_session(struct bib *db,
 	struct bib_session_tuple new;
 	struct bib_session_tuple old;
 	struct slot_group slots;
+	struct bib_delete_list rm_list = { NULL };
 	int error;
 
 	table = get_table(db, session->proto);
@@ -1746,7 +1846,7 @@ int bib_add_session(struct bib *db,
 
 	spin_lock_bh(&table->lock);
 
-	error = find_bib_session6(table, NULL, &new, &old, &slots);
+	error = find_bib_session6(table, NULL, &new, &old, &slots, &rm_list);
 	if (error)
 		goto end;
 
@@ -1766,6 +1866,7 @@ end:
 		kmem_cache_free(bib_cache, new.bib);
 	if (new.session)
 		kmem_cache_free(session_cache, new.session);
+	commit_delete_list(&rm_list);
 
 	return error;
 }
@@ -2079,6 +2180,7 @@ int bib_add_static(struct bib *db, struct bib_entry *new,
 
 	treeslot_commit(&slot6);
 	treeslot_commit(&slot4);
+	table->bib_count++;
 
 	/*
 	 * Since the BIB entry is now available, and assuming ADF is disabled,
@@ -2105,63 +2207,6 @@ eexist:
 	return -EEXIST;
 }
 
-struct detach_args {
-	struct bib_table *table;
-	unsigned int detached;
-};
-
-static void detach_session(struct rb_node *node, void *arg)
-{
-	struct tabled_session *session = node2session(node);
-	struct detach_args *args = arg;
-
-	list_del(&session->list_hook);
-	/*
-	 * Perhaps this should be an ICMP error, not a kill.
-	 * But since we're removing the mapping, we don't know whether a port
-	 * unreachable is the right message. It depends on why the user is
-	 * removing the mapping; the v4 client could very well succeed in
-	 * connecting in its next attempt, but we'll foil it if we fetch the
-	 * ICMP error.
-	 * So let's leave it at silence, which is not much different
-	 * from don't know.
-	 */
-	kill_stored_pkt(args->table, session);
-	args->detached++;
-}
-
-static unsigned int detach_sessions(struct bib_table *table,
-		struct tabled_bib *bib)
-{
-	struct detach_args arg = { .table = table, .detached = 0, };
-	rbtree_foreach(&bib->sessions, detach_session, &arg);
-	return arg.detached;
-}
-
-static void detach_bib(struct bib_table *table, struct tabled_bib *bib)
-{
-	rb_erase(&bib->hook6, &table->tree6);
-	rb_erase(&bib->hook4, &table->tree4);
-	table->bib_count--;
-	table->session_count -= detach_sessions(table, bib);
-}
-
-static void destroy_session(struct rb_node *node, void *arg)
-{
-	struct tabled_session *session = node2session(node);
-	kmem_cache_free(session_cache, session);
-}
-
-static void destroy_bib(struct tabled_bib *bib)
-{
-	/*
-	 * kmem_cache_free() is kind of bulky inside; that's the reason
-	 * why I bother with this mess after the spinlock.
-	 */
-	rbtree_clear(&bib->sessions, destroy_session, NULL);
-	kmem_cache_free(bib_cache, bib);
-}
-
 int bib_rm(struct bib *db, struct bib_entry *entry)
 {
 	struct bib_table *table;
@@ -2186,35 +2231,9 @@ int bib_rm(struct bib *db, struct bib_entry *entry)
 	spin_unlock_bh(&table->lock);
 
 	if (!error)
-		destroy_bib(bib);
+		release_bib_entry(&bib->hook4, NULL);
 
 	return error;
-}
-
-struct bib_delete_list {
-	struct rb_node *first;
-	struct rb_node *last;
-};
-
-static void add_to_delete_list(struct bib_delete_list *list, struct rb_node *node)
-{
-	if (list->first)
-		list->last->rb_right = node;
-	else
-		list->first = node;
-	list->last = node;
-	list->last->rb_right = NULL;
-}
-
-static void commit_delete_list(struct bib_delete_list *list)
-{
-	struct rb_node *node;
-	struct rb_node *next;
-
-	for (node = list->first; node; node = next) {
-		next = node->rb_right;
-		destroy_bib(bib4_entry(node));
-	}
 }
 
 void bib_rm_range(struct bib *db, l4_protocol proto, struct ipv4_range *range)

mod/stateful/pool4/db.c
@@ -56,6 +56,18 @@ struct mask_domain {
 	struct pool4_range *current_range;
 	int current_port;
 
+	/**
+	 * A "dynamic" domain is one that was generated on the fly - that is,
+	 * Jool queried the interface addresses, picked one and used it to
+	 * improvise a domain.
+	 *
+	 * A "static" domain is one the user predefined.
+	 *
+	 * Empty pool4 generates dynamic domains and populated ones generate
+	 * static domains.
+	 */
+	bool dynamic;
+
 	/*
 	 * An array of struct pool4_range hangs off here.
 	 * (The array length is @sample_count.)
@@ -851,6 +863,7 @@ static struct mask_domain *find_empty(struct route4_args *args,
 	masks->range_count = 1;
 	masks->current_range = range;
 	masks->current_port = range->ports.min + offset % masks->taddr_count;
+	masks->dynamic = true;
 	return masks;
 }
 
@@ -891,6 +904,7 @@ struct mask_domain *mask_domain_find(struct pool4 *pool, struct tuple *tuple6,
 	spin_unlock_bh(&pool->lock);
 
 	masks->taddr_counter = 0;
+	masks->dynamic = false;
 	offset %= masks->taddr_count;
 
 	foreach_domain_range(entry, masks) {
@@ -953,4 +967,9 @@ bool mask_domain_matches(struct mask_domain *masks,
 	}
 
 	return false;
+}
+
+bool mask_domain_is_dynamic(struct mask_domain *masks)
+{
+	return masks->dynamic;
 }

mod/stateless/Makefile
@@ -27,3 +27,7 @@ insert:
 remove:
 	-sudo rmmod jool_siit.ko
 	dmesg | tail -1
+again:
+	sudo rmmod jool_siit
+	make
+	sudo insmod jool_siit.ko disabled

mod/stateless/eam.c
@@ -73,9 +73,69 @@ static int validate_prefixes(struct ipv6_prefix *prefix6,
 	return 0;
 }
 
+static void msg_programming_error(void)
+{
+	log_err("(Note: This error should have been caught earlier.");
+	log_err("This looks like a bug; please report.)");
+}
+
+static int collision6(struct ipv6_prefix *prefix6, struct ipv4_prefix *prefix4,
+		struct eamt_entry *old, bool force)
+{
+	if (prefix6->len == old->prefix6.len) {
+		if (prefix4_equals(prefix4, &old->prefix4)) {
+			log_err("The requested entry already exists.");
+			return -EEXIST;
+		}
+		log_err("Existing EAM [%pI6c/%u|%pI4/%u] already contains prefix %pI6c/%u.",
+				&old->prefix6.address, old->prefix6.len,
+				&old->prefix4.address, old->prefix4.len,
+				&prefix6->address, prefix6->len);
+		return -EEXIST;
+	}
+
+	if (force)
+		return 0;
+
+	log_err("Prefix %pI6c/%u overlaps with EAM [%pI6c/%u|%pI4/%u].\n"
+			"(Use --force to override this validation.)",
+			&prefix6->address, prefix6->len,
+			&old->prefix6.address, old->prefix6.len,
+			&old->prefix4.address, old->prefix4.len);
+	return -EEXIST;
+}
+
+static int collision4(struct ipv6_prefix *prefix6, struct ipv4_prefix *prefix4,
+		struct eamt_entry *old, bool force)
+{
+	if (prefix4->len == old->prefix4.len) {
+		if (prefix6_equals(prefix6, &old->prefix6)) {
+			log_err("The requested entry already exists.");
+			msg_programming_error();
+			return -EEXIST;
+		}
+		log_err("Existing EAM [%pI6c/%u|%pI4/%u] already contains prefix %pI4/%u.",
+				&old->prefix6.address, old->prefix6.len,
+				&old->prefix4.address, old->prefix4.len,
+				&prefix4->address, prefix4->len);
+		return -EEXIST;
+	}
+
+	if (force)
+		return 0;
+
+	log_err("Prefix %pI4/%u overlaps with EAM [%pI6c/%u|%pI4/%u].\n"
+			"(Use --force to override this validation.)",
+			&prefix4->address, prefix4->len,
+			&old->prefix6.address, old->prefix6.len,
+			&old->prefix4.address, old->prefix4.len);
+	return -EEXIST;
+}
+
 static int validate_overlapping(struct eam_table *eamt,
 		struct ipv6_prefix *prefix6,
-		struct ipv4_prefix *prefix4)
+		struct ipv4_prefix *prefix4,
+		bool force)
 {
 	struct eamt_entry old;
 	struct rtrie_key key6 = PREFIX_TO_KEY(prefix6);
@@ -89,22 +149,16 @@ static int validate_overlapping(struct eam_table *eamt,
 
 	error = rtrie_get(&eamt->trie6, &key6, &old);
 	if (!error) {
-		log_err("Prefix %pI6c/%u overlaps with EAM [%pI6c/%u|%pI4/%u].\n"
-				"(Use --force to override this validation.)",
-				&prefix6->address, prefix6->len,
-				&old.prefix6.address, old.prefix6.len,
-				&old.prefix4.address, old.prefix4.len);
-		return -EEXIST;
+		error = collision6(prefix6, prefix4, &old, force);
+		if (error)
+			return error;
 	}
 
 	error = rtrie_get(&eamt->trie4, &key4, &old);
 	if (!error) {
-		log_err("Prefix %pI4/%u overlaps with EAM [%pI6c/%u|%pI4/%u].\n"
-				"(Use --force to override this validation.)",
-				&prefix4->address, prefix4->len,
-				&old.prefix6.address, old.prefix6.len,
-				&old.prefix4.address, old.prefix4.len);
-		return -EEXIST;
+		error = collision4(prefix6, prefix4, &old, force);
+		if (error)
+			return error;
 	}
 
 	return 0;
@@ -130,6 +184,7 @@ static int eamt_add6(struct eam_table *eamt, struct eamt_entry *eam)
 	if (error == -EEXIST) {
 		log_err("Prefix %pI6c/%u already exists.",
 				&eam->prefix6.address, eam->prefix6.len);
+		msg_programming_error();
 	}
 	/* rtrie_print("IPv6 trie after add", &eamt->trie6); */
 
@@ -146,6 +201,7 @@ static int eamt_add4(struct eam_table *eamt, struct eamt_entry *eam)
 	if (error == -EEXIST) {
 		log_err("Prefix %pI4/%u already exists.",
 				&eam->prefix4.address, eam->prefix4.len);
+		msg_programming_error();
 	}
 	/* rtrie_print("IPv4 trie after add", &eamt->trie4); */
 
@@ -166,11 +222,9 @@ int eamt_add(struct eam_table *eamt,
 
 	mutex_lock(&lock);
 
-	if (!force) {
-		error = validate_overlapping(eamt, prefix6, prefix4);
-		if (error)
-			goto end;
-	}
+	error = validate_overlapping(eamt, prefix6, prefix4, force);
+	if (error)
+		goto end;
 
 	new.prefix6 = *prefix6;
 	new.prefix4 = *prefix4;

test/unit/impersonator/bib.c
@@ -13,6 +13,18 @@ int mask_domain_next(struct mask_domain *masks,
 	return broken_unit_call(__func__);
 }
 
+bool mask_domain_matches(struct mask_domain *masks,
+		struct ipv4_transport_addr *addr)
+{
+	broken_unit_call(__func__);
+	return false;
+}
+
+bool mask_domain_is_dynamic(struct mask_domain *masks)
+{
+	return false;
+}
+
 struct pktqueue *pktqueue_create(void)
 {
 	return (struct pktqueue *)&dummy;