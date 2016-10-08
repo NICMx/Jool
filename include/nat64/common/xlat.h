2310c8b2d073406b20a11e46bdd03daf3d35f315
Alberto Leiva Popper <ydahhrk@gmail.com>
2016-10-07 20:14:31 -0500

    Release 3.5.1 paperwork

--- include/nat64/common/xlat.h
+++ include/nat64/common/xlat.h
@@ -12,7 +12,7 @@
  */
 #define JOOL_VERSION_MAJOR 3
 #define JOOL_VERSION_MINOR 5
-#define JOOL_VERSION_REV 0
+#define JOOL_VERSION_REV 1
 #define JOOL_VERSION_DEV 0
 
 /** See http://stackoverflow.com/questions/195975 */
