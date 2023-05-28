eb62a12b4b952215aeb5dceff1b0f3b3ed561ec4
Alberto Leiva Popper <ydahhrk@gmail.com>
2023-05-27 18:43:15 -0600

    Coexist the two test sockets

--- src/usr/dual-request/dual-request.c
+++ src/usr/dual-request/dual-request.c
@@ -8,27 +8,31 @@ static struct jool_result print_entry(struct instance_entry_usr const *instance,
 	return result_success();
 }
 
-static int display_instances(xlator_type xt)
-{
-	struct joolnl_socket sk;
-	struct jool_result result;
-
-	result = joolnl_setup(&sk, xt);
-	if (result.error)
-		return result.error;
-	result = joolnl_instance_foreach(&sk, print_entry, NULL);
-	joolnl_teardown(&sk);
-
-	return result.error;
-}
-
 int main(int argc, char **argv)
 {
+	struct joolnl_socket sk1;
+	struct joolnl_socket sk2;
+	struct jool_result result;
+
 	printf("SIIT instances:\n");
-	display_instances(XT_SIIT);
+	result = joolnl_setup(&sk1, XT_SIIT);
+	if (result.error)
+		return result.error;
+	result = joolnl_instance_foreach(&sk1, print_entry, NULL);
+	if (result.error)
+		goto revert_sk1;
 
 	printf("NAT64 instances:\n");
-	display_instances(XT_NAT64);
+	result = joolnl_setup(&sk2, XT_NAT64);
+	if (result.error)
+		goto revert_sk1;
+	result = joolnl_instance_foreach(&sk2, print_entry, NULL);
 
+	joolnl_teardown(&sk1);
+	joolnl_teardown(&sk2);
 	return 0;
+
+revert_sk1:
+	joolnl_teardown(&sk1);
+	return result.error;
 }
